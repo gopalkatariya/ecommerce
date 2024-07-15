4e412fa5a63beb66d45059a8fa07219e62fa4b31
Gopal Katariya <gopal.katariya@differenzsystem.com>
2024-07-15 11:08:47 +0530

    Switch to MySql

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -76,12 +76,15 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'ecommerce',
+		'USER': 'gopal',
+		'PASSWORD': 'ChangeMe007',
+		'HOST':'localhost',
+		'PORT':'3306',
+	}
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
